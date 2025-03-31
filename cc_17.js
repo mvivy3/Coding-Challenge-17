f1009a0a5d55f6c295a6a4232113940fb697b559
mvivy3 <meredithyen@usf.edu>
2025-03-30 21:19:55 -0400

    Task 3 - Extended VIPCustomer class

--- cc_17.js
+++ cc_17.js
@@ -41,3 +41,20 @@ class Customer {
   const client1 = new SalesRep("Mabel Sabel"); // client name
   client1.addClient(customer1);
   console.log(`Client: ${client1.name} - ${customer1.name}, Total Spent: $${client1.getClientTotal(`Cinnamon Queen`)}`);
+
+// Task 3: Create a VIPCustomer Class
+class VIPCustomer extends Customer {
+    constructor(name, email, vipLevel) {
+        super(name, email);
+        this.vipLevel = vipLevel; // string: 'Gold', 'Platinum'
+    }
+    getTotalSpent() { // override method, returns total spent with 10% loyalty bonus added
+        const baseTotal = super.getTotalSpent();
+        return baseTotal * 1.10;
+    }
+}
+const vipCustomer1 = new VIPCustomer('Harper Violet', 'harperviolet@gmail.com', 'Platinum');
+vipCustomer1.addPurchase(100); // purchase amount
+vipCustomer1.addPurchase(200); // purchase amount
+console.log(`VIP Customer: ${vipCustomer1.name},  Level: ${vipCustomer1.vipLevel} - Bonus Total: $${vipCustomer1.getTotalSpent().toFixed(2)}`);
+
