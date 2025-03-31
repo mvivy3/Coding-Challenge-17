29b09d4d496eb37926bc66982213f812f0603c07
mvivy3 <meredithyen@usf.edu>
2025-03-30 21:19:15 -0400

    Task 2 - Created SalesRep class

--- cc_17.js
+++ cc_17.js
@@ -19,4 +19,25 @@ class Customer {
   customer1.addPurchase(50); // purchase amount
  console.log(`Customer: ${customer1.name}, Total Spent: $${customer1.getTotalSpent()}`);
 
-   
+ // Task 2: Create a SalesRep Class
+  class SalesRep {
+    constructor(name) {
+        this.name = name; // string
+        this.clients = []; // array of customer objects
+    }
+    addClient(customer) { // adds a customer to the list
+        this.clients.push(customer);
+    }
+    getClientTotal(clientName) { // finds client by name
+        const client = this.clients.find(client => client.name === clientName);
+        if (client) {
+            return client.getTotalSpent(); // returns total spent
+        } else {
+            console.error(`Client ${clientName} not found`);
+            return 0;
+        }
+    }
+  }
+  const client1 = new SalesRep("Mabel Sabel"); // client name
+  client1.addClient(customer1);
+  console.log(`Client: ${client1.name} - ${customer1.name}, Total Spent: $${client1.getClientTotal(`Cinnamon Queen`)}`);
